cc407e0073a2253cb48f04fcf446b728bbd64f7f
Daniel Antunes Ribeiro <38988105+dielito2010@users.noreply.github.com>
2023-05-11 19:15:54 -0400

    add lib glid from project

--- app/app/src/main/java/br/dev/danielribeiro/bestage60/FunatiNewsActivity.kt
+++ app/app/src/main/java/br/dev/danielribeiro/bestage60/FunatiNewsActivity.kt
@@ -1,15 +1,24 @@
 package br.dev.danielribeiro.bestage60
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class FunatiNewsActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_funati_news)
+
+        var imgFunati1 = findViewById<ImageView>(R.id.imgFunati1)
+        Glide.with(this)
+            .load("https://funati.am.gov.br/wp-content/uploads/2019/10/Atividades-2-1920x1610.jpg")
+            .into(imgFunati1)
 
         var btnWebFunati = findViewById<Button>(R.id.btnWebFunati)
         btnWebFunati.setOnClickListener {
